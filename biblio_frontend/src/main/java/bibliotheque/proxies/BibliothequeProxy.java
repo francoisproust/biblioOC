f8d442967f81526379355deb56c961b485b7c3e6
francois <francois.proust.72@gmail.com>
2020-10-06 13:22:10 +0200

    vérification que tout est ok, correction config frontend

--- biblio_frontend/src/main/java/bibliotheque/proxies/BibliothequeProxy.java
+++ biblio_frontend/src/main/java/bibliotheque/proxies/BibliothequeProxy.java
@@ -1,6 +1,5 @@
 package bibliotheque.proxies;
 
-import bibliotheque.bibliofront.modele.*;
 import bibliotheque.modele.*;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
