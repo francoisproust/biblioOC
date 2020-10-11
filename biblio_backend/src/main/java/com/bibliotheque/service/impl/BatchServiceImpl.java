eb1535b414dabcc35413265357a3dc243fc49721
francois <francois.proust.72@gmail.com>
2020-10-11 16:19:57 +0200

    envoi du mail au suivant, ajout dateAlerte sur la reservation suivante

--- biblio_backend/src/main/java/com/bibliotheque/service/impl/BatchServiceImpl.java
+++ biblio_backend/src/main/java/com/bibliotheque/service/impl/BatchServiceImpl.java
@@ -3,6 +3,7 @@ package com.bibliotheque.service.impl;
 import com.bibliotheque.modele.dao.ReserverDao;
 import com.bibliotheque.modele.entities.Reserver;
 import com.bibliotheque.service.BatchService;
+import com.bibliotheque.service.ReserverService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
@@ -11,16 +12,37 @@ import java.util.List;
 public class BatchServiceImpl implements BatchService {
     @Autowired
     ReserverDao reserverDao;
+    @Autowired
+    ReserverService reserverService;
 
     @Override
     public void refreshReservation() {
-        Date dateDuJour = new Date();
         List<Reserver> reservation = reserverDao.findAll();
-        for(int i=0;i<reservation.size();i++){
-            Date dateAlerte = reservation.get(i).getDateAlerte();
+        suppressionResaDepasses(reservation);
+        envoiMail(reservation);
+        ajoutDateAlerte(reservation);
+    }
+
+    private void suppressionResaDepasses(List<Reserver> reservers){
+        Date dateDuJour = new Date();
+        for(int i=0;i<reservers.size();i++){
+            Date dateAlerte = reservers.get(i).getDateAlerte();
             if(dateAlerte.compareTo(dateDuJour) > 2){
-                reserverDao.deleteByReserverId(reservation.get(i).getReserverId());
+                reserverDao.deleteByReserverId(reservers.get(i).getReserverId());
             }
+        }
+    }
+
+    private void envoiMail(List<Reserver> reservers){
+        for (int i = 0;i<reservers.size();i++){
+            reserverService.sendMailToMember(reservers.get(i));
+        }
+    }
+
+    private void ajoutDateAlerte(List<Reserver> reservers){
+        Date dateAlerte = new Date();
+        for (int i = 0;i<reservers.size();i++){
+            reservers.get(i).setDateAlerte(dateAlerte);
         }
     }
 }
